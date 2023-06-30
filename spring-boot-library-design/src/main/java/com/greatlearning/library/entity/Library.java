336584c82e610ca1a42cdc0c1d8291757d8d80e1
arunayesaswini <127669982+arunayesaswini@users.noreply.github.com>
2023-06-30 17:17:33 +0530

    Add files via upload

---
+++ spring-boot-library-design/src/main/java/com/greatlearning/library/entity/Library.java
@@ -0,0 +1,21 @@
+package com.greatlearning.library.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Library 
+{
+	@Id
+	private long id;
+	private String name;
+	private String commaSeperatedBooknames;
+
+}
